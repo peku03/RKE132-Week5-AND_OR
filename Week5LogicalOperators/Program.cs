1b030d78b7034329695adf04e465812dec08107f
unknown <markus.pekkenen@gmail.com>
2024-03-11 12:51:18 +0200

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,13 @@
+﻿//AND - &&
+
+//ture && true && true --> true
+//true && flase --> false
+//fasle && true --> false
+//false && flase --> false
+
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true
+//flse || flase --> false
